c7f7c95235c358269284a13012e1f79b318d680f
Andrew Rove <andrew@realsavvy.com>
2018-02-16 14:29:11 -0600

    Change dist name and main file target for package.

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const MinifyPlugin = require('babel-minify-webpack-plugin');
 
 module.exports = {
   entry: {
-    'real_savvy_client': './src/index.js',
-    'real_savvy_client.min': './src/index.js',
+    'real_savvy': './src/index.js',
+    'real_savvy.min': './src/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
